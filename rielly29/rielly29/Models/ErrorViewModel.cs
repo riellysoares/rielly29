56205c0f7524ddc208f8e1e11181bb9b9727facf
riellysoares <rielly.soares@aluno.senai.br>
2026-04-27 19:48:52 -0300

    Setup Inicial MVC

---
+++ rielly29/rielly29/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace rielly29.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
