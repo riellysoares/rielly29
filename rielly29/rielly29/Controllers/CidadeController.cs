da360058bb313f12ff5e82be7fbe3aa088788b18
riellysoares <rielly.soares@aluno.senai.br>
2026-04-27 20:41:21 -0300

    Finalizando com CidadeController e Views

---
+++ rielly29/rielly29/Controllers/CidadeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rielly29.Controllers
+{
+    public class CidadeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detalhes()
+        {
+            return View();
+        }
+    }
+}
--- rielly29/rielly29/obj/Debug/net10.0/rielly29.AssemblyInfo.cs
+++ rielly29/rielly29/obj/Debug/net10.0/rielly29.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rielly29")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f78a1bc920b94f015fd81ef51f4350f0975484a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3fb6027fd2331fec8f873880bc2d1bd6f562b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("rielly29")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rielly29")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
